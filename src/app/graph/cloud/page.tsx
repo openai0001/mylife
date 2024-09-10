b6ddcc504e3aa6d4f77a7ae147bf226bf91e42c1
youht <youht@youhtdeMac-mini.local>
2024-09-10 18:59:45 +0800

    update

---
+++ src/app/graph/cloud/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="border-2 border-red-500 p-4">
+            <h1> hello world</h1>
+        </div>
+    )
+}
--- src/app/graph/test/page.tsx
+++ src/app/graph/test/page.tsx
@@ -4,20 +4,24 @@
 import { HumanMessage } from '@langchain/core/messages';
 import { RemoteRunnable } from "@langchain/core/runnables/remote";
 import { Typography } from 'antd';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 const { Paragraph, Title } = Typography;
 
 export default function Home() {
-  const router = useRouter();
   const pathname = usePathname();
   const [markdownContent, setMarkdownContent] = useState<string>('');
+  
+  const [data, setData] = useState<any[]>([])
   const bufferRef = useRef<string>(''); // 使用 ref 存储当前的内容
+  let last_input = ""
   useEffect(() => {
     const url = new URL(window.location.href)
     const input = url.searchParams.get('input');
-    if (!input) return;
+    console.log("hello??",input)
+    if (!input || input == last_input) return;
+    //last_input = input
     const fetchData = async () => {
       try {
         const remoteChain = new RemoteRunnable({
@@ -26,12 +30,17 @@ export default function Home() {
         const messages = [new HumanMessage({ content: input })]
         const eventStream = remoteChain.streamEvents({ "messages": messages }, {
           version: "v1",
+          configurable: {
+            thread_id: "youht-default",
+          },
         });
-        let chunks = ""
         const node_set = new Set()
         for await (const event of eventStream) {
+          console.log(event)
           if (event.event === "on_chain_stream" && event.data.chunk.messages) {
-            console.log(node_set,event.name)
+            data.push({ "id": event.run_id, "event": event.event, "name": event.name, "content": event.data.chunk.messages[0].content })
+            setData(data)
+            console.log(node_set, event.name)
             if (!node_set.has(event.name)) {
               node_set.add(event.name)
               console.log(event.data.chunk.messages[0].content, event.name)
@@ -40,7 +49,20 @@ export default function Home() {
             }
           }
         }
-
+        const res = await remoteChain.stream({ "messages": messages }, {
+          configurable: {
+            thread_id: "youht-default",
+          },
+        });
+        let chunks = ""
+        for await (const chunk of res){
+            console.log(chunk,typeof(chunk),Object.keys(chunk as object))
+            const id = data.length
+            const key = Object.keys(chunk as object)[0]
+            const chunk_key = chunk[key]
+            const item = {"id": id ,"content": chunk_key.messages[0].content }
+            setData((prevItems)=>[...prevItems,item])  
+        }
       } catch (error) {
         console.log("error fetching data", error);
       }
@@ -58,8 +80,11 @@ export default function Home() {
   return (
     <div>
       <Typography.Title level={2}>Streaming Response:</Typography.Title>
+      {data.length}
       <div style={containerStyle}>
-        <ReactMarkdown>{markdownContent}</ReactMarkdown>
+        <ul>
+          {data.map(item => (<li key={(item.id)}><ReactMarkdown>{item.content}</ReactMarkdown></li>))}
+        </ul>
       </div>
     </div>
   );
