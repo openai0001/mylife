479759049911f32557fa15ff58359fc8bcd2fae3
youht <youht@youhtdeMac-mini.local>
2024-09-10 19:01:53 +0800

    update

--- src/app/graph/cloud/page.tsx
+++ src/app/graph/cloud/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="border-2 border-red-500 p-4">
+        <div className="border-2 border-red-500 p-2 m-2">
             <h1> hello world</h1>
         </div>
     )
